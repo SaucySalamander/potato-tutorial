4c3120dcfc70d11b1b99789ffb5c4723204711aa
SaucySalamander <nick.sfiroudis@gmail.com>
2020-05-06 20:01:43 -0500

    got creating a new window working properly by swithing to device event instead of window event.

--- src/window.rs
+++ src/window.rs
@@ -1,11 +1,9 @@
 use std::collections::HashMap;
-use std::time::{Instant, Duration};
 use winit::{
-    event::{ElementState, Event, KeyboardInput, WindowEvent},
+    event::{DeviceEvent, ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::Window,
 };
-use std::thread::sleep;
 
 pub fn init() {
     simple_logger::init().unwrap();
@@ -20,30 +18,31 @@ pub fn init() {
 
         match event {
             Event::WindowEvent { event, window_id } => {
-                match event {
-                    WindowEvent::CloseRequested => {
-                        println!("Window {:?} has received the signal to close", window_id);
-
-                        // This drops the window, causing it to close.
-                        windows.remove(&window_id);
-
-                        if windows.is_empty() {
-                            *control_flow = ControlFlow::Exit;
-                        }
+                if let WindowEvent::CloseRequested = event {
+                    println!("Window {:?} has received the signal to close", window_id);
+                    windows.remove(&window_id);
+                    if windows.is_empty() {
+                        *control_flow = ControlFlow::Exit;
                     }
-                    WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                state: ElementState::Pressed,
-                                ..
-                            },
-                        ..
-                    } => {
+                }
+            }
+            Event::DeviceEvent { event, .. } => {
+                if let DeviceEvent::Key(KeyboardInput {
+                    virtual_keycode,
+                    state,
+                    ..
+                }) = event
+                {
+                    if state == ElementState::Released && virtual_keycode == Some(VirtualKeyCode::N)
+                    {
                         let window = Window::new(&event_loop).unwrap();
                         windows.insert(window.id(), window);
-                        sleep(Duration::from_millis(100));
                     }
-                    _ => (),
+                }
+            }
+            Event::MainEventsCleared => {
+                for (.., window) in windows.iter() {
+                    window.request_redraw();
                 }
             }
             _ => (),
