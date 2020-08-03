2eee6f16eb9536feff3d0ad0f36fbad6fe21bc03
Saucy Salamander <nick.sfiroudis@gmail.com>
2020-08-03 00:57:12 -0500

    Setup for graphic pipeline construction (#6)
    * Added logic to destroy image views and added stub function for graphics pipeline
    
    * Adding new github action
    
    * fixing syntax in github action
    
    * Trying to get the syntax right on my github actions :/

--- src/vulkan/vulk_init.rs
+++ src/vulkan/vulk_init.rs
@@ -132,6 +132,7 @@ impl VulkanApiObjects {
 impl Drop for VulkanApiObjects {
     fn drop(&mut self) {
         unsafe {
+            self.swapchain.swapchain_image_views.iter().for_each(|x| self.device.destroy_image_view(*x, None));
             self.swapchain.swapchain_loader.destroy_swapchain(self.swapchain.swapchain, None);
             self.device.destroy_device(None);
             self.surface_loader.destroy_surface(self.surface, None);
